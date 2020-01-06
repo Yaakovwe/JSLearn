40e24d8a1f7465e473dbd6ea887f489547ef8ffd
Y <Y@W>
2020-01-06 10:17:57 +0200

    After CR with Ohad

--- diamond.js
+++ diamond.js
@@ -1,38 +1,36 @@
-function diamond(num, char) {
-    specialChar = char;
-    space = " ";
-    newSpace = " ";
-    arrSpaces = [];
-    asterixArr = [];
-    for (let i = 0; i < num; i++) {
+"use strict";
+
+function diamond(size, char) {
+    let specialChar = char;
+    const space = " ";
+    let newSpace = " ";
+    const arrSpaces = [];
+    const asterixArr = [];
+
+    for (let i = 0; i < size * 2; i++) {
         if (i % 2 === 0) {
             asterixArr.push(specialChar);
-        }
-        specialChar += char;
-    }
-    for (let j = 0; j < num; j++) {
-        if (j % 2 === 0) {
             arrSpaces.push(newSpace);
             newSpace += space;
         }
+        specialChar += char;
     }
 
     arrSpaces.reverse();
     // console.log(arrSpaces);
     // console.log(asterixArr);
-    for (let k = 0; k < num / 2; k++) {
+    for (let k = 0; k < size; k++) {
         console.log(arrSpaces[k] + asterixArr[k]);
     }
+
     asterixArr.pop();
     arrSpaces.pop();
     asterixArr.reverse();
     arrSpaces.reverse();
     // console.log(asterixArr);
     //console.log(arrSpaces);
-    for (let l = 0; l < num / 2; l++) {
-        if (arrSpaces !== NaN && asterixArr !== NaN) {
-            console.log(arrSpaces[l] + asterixArr[l]);
-        }
+    for (let l = 0; l < size - 1; l++) {
+        console.log(arrSpaces[l] + asterixArr[l]);
     }
     //console.log(lastSpace + char);
 }
