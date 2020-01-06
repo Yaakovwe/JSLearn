7da882f55085a33f7b79a12eee4d5462e55c23cb
Y <Y@W>
2020-01-06 11:11:09 +0200

    1) Added argument to function for Space (spacheHolder) 2) changed arrSpaces to spacesArr 3) Changed loop variables to i (they are in different scopes)

--- diamond.js
+++ diamond.js
@@ -1,38 +1,38 @@
 "use strict";
 
-function diamond(size, char) {
+function diamond(size, char, spaceHolder) {
     let specialChar = char;
-    const space = " ";
-    let newSpace = " ";
-    const arrSpaces = [];
+    const space = spaceHolder;
+    let newSpace = spaceHolder;
+    const spacesArr = [];
     const asterixArr = [];
-
+    // if (typeof((size) === 'string')))
     for (let i = 0; i < size * 2; i++) {
         if (i % 2 === 0) {
             asterixArr.push(specialChar);
-            arrSpaces.push(newSpace);
+            spacesArr.push(newSpace);
             newSpace += space;
         }
         specialChar += char;
     }
 
-    arrSpaces.reverse();
+    spacesArr.reverse();
     // console.log(arrSpaces);
     // console.log(asterixArr);
-    for (let j = 0; j < size; j++) {
-        console.log(arrSpaces[j] + asterixArr[j]);
+    for (let i = 0; i < size; i++) {
+        console.log(spacesArr[i] + asterixArr[i]);
     }
 
     asterixArr.pop();
-    arrSpaces.pop();
+    spacesArr.pop();
     asterixArr.reverse();
-    arrSpaces.reverse();
+    spacesArr.reverse();
     // console.log(asterixArr);
     //console.log(arrSpaces);
-    for (let k = 0; k < size - 1; k++) {
-        console.log(arrSpaces[k] + asterixArr[k]);
+    for (let i = 0; i < size - 1; i++) {
+        console.log(spacesArr[i] + asterixArr[i]);
     }
     //console.log(lastSpace + char);
 }
 
-diamond(4, "*")
+diamond(4, "*", " ")
