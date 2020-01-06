8dc8b9f1204e919ed3eb80655a2454f6c64d1ec6
Y <Y@W>
2020-01-06 12:02:01 +0200

    Added tests

--- diamond.js
+++ diamond.js
@@ -34,13 +34,39 @@ function diamond(size, char, spaceHolder) {
     //console.log(lastSpace + char);
 }
 
+//console.log(diamond(8, "*", " "));
+
 console.log(0);
-diamond(0, "*", " ");
+diamond(0, "*", " "); //OK - 0 *
 console.log(1);
-diamond(1, "*", " ");
+diamond(1, "*", " "); //OK - 2 *
 console.log(-1);
-diamond(-1, "*", " ");
+diamond(-1, "*", " "); //ERROR
 console.log(2);
-diamond(2, "*", " ");
+diamond(2, "*", " "); //OK
 console.log(4);
-diamond(4, "*", " ");
+diamond(4, "*", " "); //OK
+
+console.log("just number"); //OK
+diamond(4);
+console.log("just char"); //ERROR
+diamond("*");
+console.log("empty"); //ERROR
+diamond();
+console.log("empty string"); //ERROR
+diamond("");
+console.log("space string"); //ERROR
+diamond(" ");
+
+console.log("num, null, null");
+diamond(4, null, null); //ERROR
+console.log("num, null, null");
+diamond(4, undefined, undefined); //OK
+console.log("num, null, null");
+diamond(4, undefined, null); //ERROR
+console.log("num, null, null");
+diamond(4, null, undefined); //ERROR
+console.log("num, null, null");
+diamond(null, null, undefined); //ERROR
+console.log("num, null, null");
+diamond(undefined, null, undefined); //ERROR
