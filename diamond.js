b899c7adb580523c200eddc2a1764febd04972d5
Y <Y@W>
2020-01-06 11:23:59 +0200

    removed the space constant and used spaceHolder argument instead renamed asterixArr to charArr

--- diamond.js
+++ diamond.js
@@ -2,16 +2,15 @@
 
 function diamond(size, char, spaceHolder) {
     let specialChar = char;
-    const space = spaceHolder;
     let newSpace = spaceHolder;
     const spacesArr = [];
-    const asterixArr = [];
+    const charArr = [];
     // if (typeof((size) === 'string')))
     for (let i = 0; i < size * 2; i++) {
         if (i % 2 === 0) {
-            asterixArr.push(specialChar);
+            charArr.push(specialChar);
             spacesArr.push(newSpace);
-            newSpace += space;
+            newSpace += spaceHolder;
         }
         specialChar += char;
     }
@@ -20,19 +19,28 @@ function diamond(size, char, spaceHolder) {
     // console.log(arrSpaces);
     // console.log(asterixArr);
     for (let i = 0; i < size; i++) {
-        console.log(spacesArr[i] + asterixArr[i]);
+        console.log(spacesArr[i] + charArr[i]);
     }
 
-    asterixArr.pop();
+    charArr.pop();
     spacesArr.pop();
-    asterixArr.reverse();
+    charArr.reverse();
     spacesArr.reverse();
     // console.log(asterixArr);
     //console.log(arrSpaces);
     for (let i = 0; i < size - 1; i++) {
-        console.log(spacesArr[i] + asterixArr[i]);
+        console.log(spacesArr[i] + charArr[i]);
     }
     //console.log(lastSpace + char);
 }
 
-diamond(4, "*", " ")
+console.log(0);
+diamond(0, "*", " ");
+console.log(1);
+diamond(1, "*", " ");
+console.log(-1);
+diamond(-1, "*", " ");
+console.log(2);
+diamond(2, "*", " ");
+console.log(4);
+diamond(4, "*", " ");
