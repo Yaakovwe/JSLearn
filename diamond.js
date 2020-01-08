041c528758fbca0ba170f9959092d7aeec86f8da
Y <Y@W>
2020-01-08 15:17:12 +0200

    Added validations

--- diamond.js
+++ diamond.js
@@ -1,77 +1,81 @@
 "use strict";
 
-function diamondFunc(size, char, spaceHolder) {
-    let specialChar = char;
-    let newSpace = spaceHolder;
-    const spacesArr = [];
-    const charArr = [];
-    const diamondArr = [];
-    let diamondVar = "";
-    // if (typeof((size) === 'string')))
-    for (let i = 0; i < size * 2; i++) {
-        if (i % 2 === 0) {
-            charArr.push(specialChar);
-            spacesArr.push(newSpace);
-            newSpace += spaceHolder;
+function diamondFunc(size, charOrString, spaceHolder) {
+    if (charOrString === undefined) {
+        charOrString = "*";
+    }
+
+    if (spaceHolder === undefined) {
+        spaceHolder = " ";
+    }
+    if (typeof size === "number") {
+        let specialChar = charOrString;
+        let newSpace = spaceHolder;
+        const spacesArr = [];
+        const charArr = [];
+        const diamondArr = [];
+        for (let i = 0; i < size * 2; i++) {
+            if (i % 2 === 0) {
+                charArr.push(specialChar);
+                spacesArr.push(newSpace);
+                newSpace += spaceHolder;
+            }
+            specialChar += charOrString;
         }
-        specialChar += char;
+
+        spacesArr.reverse();
+        // console.log(arrSpaces);
+        // console.log(asterixArr);
+        for (let i = 0; i < size; i++) {
+            diamondArr.push(spacesArr[i] + charArr[i]);
+        }
+        charArr.pop();
+        spacesArr.pop();
+        charArr.reverse();
+        spacesArr.reverse();
+        // console.log(asterixArr);
+        //console.log(arrSpaces);
+        for (let i = 0; i < size - 1; i++) {
+            diamondArr.push(spacesArr[i] + charArr[i]);
+        }
+        return diamondArr.join("\n");
+    } else {
+        return "Size must be a number\n" + "charOrString must be a char or a string\n" + "spaceHolder must be a string";
     }
 
-    spacesArr.reverse();
-    // console.log(arrSpaces);
-    // console.log(asterixArr);
-    for (let i = 0; i < size; i++) {
-        diamondArr.push(spacesArr[i] + charArr[i]);
-    }
-    charArr.pop();
-    spacesArr.pop();
-    charArr.reverse();
-    spacesArr.reverse();
-    // console.log(asterixArr);
-    //console.log(arrSpaces);
-    for (let i = 0; i < size - 1; i++) {
-        diamondArr.push(spacesArr[i] + charArr[i]);
-    }
-    for (let i = 0; i < diamondArr.length; i++) {
-        diamondVar += diamondArr[i] + ("\n");
-    }
-
-    return diamondVar;
 }
-
-console.log(diamondFunc(5, "*", " ")); //OK
-
-console.log(0); // OK
-diamond(0, "*", " "); //OK - 0 *
-// console.log(1);
-// diamond(1, "*", " "); //OK - 2 *
+console.log(diamondFunc(8)); // OK (15 lines)
+console.log(diamondFunc(0)); // OK (No lines)
+console.log(diamondFunc('#')); // ("Size must be a number")
+console.log(diamondFunc(3)); // OK (5 lines)
+//console.log(1);
 // console.log(-1);
-// diamond(-1, "*", " "); //ERROR
+// console.log(diamondFunc(-1, "*", " "); //ERROR
 // console.log(2);
-// diamond(2, "*", " "); //OK
+// console.log(diamondFunc(2, "*", " "); //OK
 // console.log(4);
-// diamond(4, "*", " "); //OK
+// console.log(diamondFunc(4, "*", " "); //OK
 
 // console.log("just number"); //OK
-// diamond(4);
+// console.log(diamondFunc(4);
 // console.log("just char"); //ERROR
-// diamond("*");
+// console.log(diamondFunc("*");
 // console.log("empty"); //ERROR
-// diamond();
+// console.log(diamondFunc();
 // console.log("empty string"); //ERROR
-// diamond("");
+// console.log(diamondFunc("");
 // console.log("space string"); //ERROR
-// diamond(" ");
+// console.log(diamondFunc(" ");
 
 // console.log("num, null, null");
-// diamond(4, null, null); //ERROR
+// console.log(diamondFunc(4, null, null); //ERROR
 // console.log("num, null, null");
-// diamond(4, undefined, undefined); //OK
+// console.log(diamondFunc(4, undefined, undefined); //OK
 // console.log("num, null, null");
-// diamond(4, undefined, null); //ERROR
+// console.log(diamondFunc(4, undefined, null); //ERROR
 // console.log("num, null, null");
-// diamond(4, null, undefined); //ERROR
+// console.log(diamondFunc(4, null, undefined); //ERROR
 // console.log("num, null, null");
-// diamond(null, null, undefined); //ERROR
+// console.log(diamondFunc(null, null, undefined); //ERROR
 // console.log("num, null, null");
-// diamond(undefined, null, undefined); //ERROR
+// console.log(diamondFunc(undefined, null, undefined); //ERROR
