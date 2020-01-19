03c17534a64b74720605560beb9775150e21e027
Y <Y@W>
2020-01-19 12:36:46 +0200

    Head node is attached to the rest of the nodes

--- linkedList.js
+++ linkedList.js
@@ -1,21 +1,28 @@
 'use strict';
-
+const util = require('util');
 class Node {
     constructor(value, next = null) {
-        this.value = value,
-            this.next = next
+        this.value;
+        this.next;
     }
 }
 
 class LinkedList {
-    constructor(arr) {
-        this.head = new Node();
-        this.head.value = arr[0];
-        this.head.next = new Node();
-        arr.forEach(element => { //number
-            this.head.next.value = element;
-            this.head.next.next = new Node();
-        });
+    constructor(array) {
+        // this.head = new Node();
+        // this.head.value = array[i];
+        //     this.head.next = null;//this.head;
+        for (var i = 0; i < array.length; i++) { //number
+            let newNode = new Node();
+            newNode.value = array[i];
+            newNode.next = null;
+            if (i == 0) {
+                this.head = newNode;
+            } else {
+                this.current.next = newNode;
+            }
+            this.current = newNode;
+        }
     }
 }
 
@@ -24,14 +31,14 @@ function myProgram(data) {
     const linkedList = new LinkedList(data);
     return linkedList.head;
 
-
-    //myLinkedList('yakov', 4, 'a'); //['y','a','k','o','v'],2,'a' => yaakov
+    //myLinkedList(['y','a','k','o','v']); //['y','a','k','o','v'],2,'a' => yaakov
     //['y','a','k','o','v'],2,'a' => yaakov
     // var mylist2 = myLinkedList([1, 2, 3, 4, 6], 4, 5); //['y','a','k','o','v'],2,'a' => yaakov
     // myLinkedList(mylist1, 4, 5); //['y','a','k','o','v'],2,'a' => yaakov
     // node = new node(4, 5);
     // mylist.add(node);
     // mylist.remove(node);
-}
 
-console.log(myProgram([1, 2, 3, 4, 6]));
+}
+console.log(util.inspect(myProgram([10, 20, 30, 40]), false, null, true /* enable colors */ ));
+console.log(util.inspect(myProgram(['y', 'a', 'k', 'o', 'v']), false, null, true /* enable colors */ )); //['y','a','k','o','v'],2,'a' => yaakov
