66c3d4f451d78eabdfd2a808072b30f54ef7e682
Y <Y@W>
2020-01-21 08:39:00 +0200

    Initial commit

---
+++ linkedListOldWay.js
@@ -0,0 +1,20 @@
+const util = require('util');
+
+
+function arrayToList(array) {
+    let list = null;
+    const arrReversed = array.reverse();
+    for (var i = 0; i < arrReversed.length; i++) {
+        // console.log(i); //2, then 1
+        // console.log(array[i]); //20, then 10
+        list = {
+            value: array[i],
+            rest: list //null, then {value:20, rest: null}
+        };
+        // console.log(list);
+    }
+    return list;
+}
+//console.dir(arrayToList([10, 20, 30, 40]));
+console.log(util.inspect(arrayToList([10, 20, 30, 40]), false, null, true /* enable colors */ ))
+    //{ value: 10, rest: { value: 20, rest: null } }
