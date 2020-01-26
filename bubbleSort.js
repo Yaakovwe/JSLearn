602bd62096015abb7b7e821eb35190e99326a4ee
Y <Y@W>
2020-01-26 17:45:08 +0200

    Fix while with swap

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,21 +3,22 @@ const util = require('util');
 
 function bubbleSort(data) {
     let temp;
-    let swap = false;
-    while (swap === false) {
+    //const size = data.length;
+    let swap = true;
+    while (swap === true) {
+        swap = false;
         for (let i = 0; i < data.length; i++) {
             if (data[i] < data[i - 1]) {
                 temp = data[i - 1];
                 data[i - 1] = data[i];
                 data[i] = temp;
-                swap = false;
-                console.log(data + " " + swap);
-            } else {
-                swap === true;
+                swap = true;
+                console.log(data + " " + swap + " " + i);
             }
         }
     }
-    return data;
+
+    return data + " " + swap;
 }
 let myArr = [5, 3, 1, 7, 9, 2, 4, 6, 8];
 console.log(util.inspect(bubbleSort(myArr), false, null, true /* enable colors */ ));
