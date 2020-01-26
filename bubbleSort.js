1936c0633374413e0c1af7efea103c4dafeee222
Y <Y@W>
2020-01-26 14:12:56 +0200

    Commit infinite loop

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,20 +2,22 @@
 const util = require('util');
 
 function bubbleSort(data) {
-    let temp = false;
-    while (temp === false) {
-        for (var i = 0; i < data.length; i++) {
+    let temp;
+    let swap = false;
+    while (swap === false) {
+        for (let i = 0; i < data.length; i++) {
             if (data[i] < data[i - 1]) {
+                temp = data[i - 1];
                 data[i - 1] = data[i];
-                console.log(data);
-                temp === false;
+                data[i] = temp;
+                swap = false;
+                console.log(data + " " + swap);
             } else {
-                temp === true;
+                swap === true;
             }
         }
     }
     return data;
 }
-
 let myArr = [5, 3, 1, 7, 9, 2, 4, 6, 8];
-console.log(util.inspect(bubbleSort(myArr, 10), false, null, true /* enable colors */ ));
+console.log(util.inspect(bubbleSort(myArr), false, null, true /* enable colors */ ));
