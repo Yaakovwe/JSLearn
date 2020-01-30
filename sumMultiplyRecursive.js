897755fbab01a06571ab940f81fecc336d98222e
Y <Y@W>
2020-01-30 08:19:36 +0200

    Chnaged functions to arrow functions

--- sumMultiplyRecursive.js
+++ sumMultiplyRecursive.js
@@ -1,5 +1,5 @@
 //with loop multiplication function without *
-// function withLoop(num1, num2) {
+// const withLoop = (num1, num2) => {
 //     let multipledNum = 0;
 //     for (let i = 0; i < num2; i++) {
 //         multipledNum += num1;
@@ -9,16 +9,19 @@
 
 // console.log(withLoop(3, 7));
 // console.log(withLoop(2, 5));
+// console.log(withLoop(8, 9));
 
 //recursive multiplication function without *
 
-
-function recursiveMultiplication(num, num2) {
+const recursiveMultiplication = (num, num2) => {
     if (num <= 0) {
         return num2;
+    } else {
+        return recursiveMultiplication(num - 1, num2 + num2);
     }
-    return recursiveMultiplication(num - 1, num2 + num2);
 }
-//console.log(recursiveMultiplication(3, 2));
+
+
+console.log(recursiveMultiplication(3, 2));
 console.log(recursiveMultiplication(5, 5));
 console.log(recursiveMultiplication(7, 8));
