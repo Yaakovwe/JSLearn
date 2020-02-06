b7113029c5c78768fec106f764907e59f63b4ecd
Y <Y@W>
2020-02-06 12:46:48 +0200

    Initial commit

---
+++ mergeSort.js
@@ -0,0 +1,18 @@
+const mergeSort = (array) => {
+        const middle = array.length / 2;
+        if (middle <= 1) {
+            return array;
+        }
+        else{
+          
+        }
+    }
+    // const mergeSortRecursive = (array, num) => {
+    //     if (num <= 0) {
+    //         return (num);
+    //     } else {
+
+//     }
+// }
+
+mergeSort([38, 27, 43, 9, 82, 10]);
